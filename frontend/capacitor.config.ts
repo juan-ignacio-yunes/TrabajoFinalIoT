bcf39df999e2c4584d6a9bb0fc319c09cd387f86
JIY <jignacio.yl@gmail.com>
2025-02-14 14:18:21 -0300

    cambio en campo name de capacitor.config.ts

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'frontend-dam',
+  appName: 'frontend-trabajoFinalIoT',
   webDir: 'www',
   server: {
     androidScheme: 'https'
