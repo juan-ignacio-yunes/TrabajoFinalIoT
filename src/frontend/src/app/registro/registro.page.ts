2e67a4c68df0e0c8660af096efcd43435f61c98e
JuanVM <jignacio.yl@gmail.com>
2025-03-09 04:07:18 -0300

    modificaciones de la págian registro

--- src/frontend/src/app/registro/registro.page.ts
+++ src/frontend/src/app/registro/registro.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Router } from '@angular/router'; // si quieres redireccionar
+import { environment } from 'src/environments/environment'; // si tienes un environment.ts
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage {
 
-  constructor() { }
+  // Objeto que enlazamos con el form (vía [(ngModel)])
+  formData = {
+    user_email: '',
+    password: '',
+    user_nombre: ''
+  };
 
-  ngOnInit() {
+  constructor(
+    private http: HttpClient,
+    private router: Router //,ngOnInit() {}
+  ) { }
+
+  onSubmit() {
+    // Suponiendo que el backend corre en: http://localhost:3000/api/registro/register
+    this.http.post('http://localhost:3000/api/registro/register', this.formData)
+      .subscribe({
+        next: (res) => {
+          console.log('Usuario registrado', res);
+          // Si quieres redirigir al login, por ejemplo:
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error('Error en registro:', err);
+          // Manejo de error, ej. mostrar un toast
+        }
+      });
   }
 
 }
