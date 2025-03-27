046f40daad9ad52b75a1a1773c86e1c2bed46e52
JuanVM <jignacio.yl@gmail.com>
2025-03-27 00:25:55 -0300

    agregué cartel de credenciales inválidas en login

--- app/src/frontend/src/app/login/login.ts
+++ app/src/frontend/src/app/login/login.ts
@@ -10,14 +10,37 @@ import { LoginService } from '../services/login.service';
 export class LoginPage {
   login = { user_email: '', password: '' };
   submitted = false;
+  errorMessage = ''; // para mostrar el error en la vista
 
   constructor(private _loginService: LoginService) { }
 
-  onLogin(form: NgForm) {
+  /* onLogin(form: NgForm) {
     this.submitted = true
   
     if (form.valid) {
       this._loginService.login(this.login.user_email, this.login.password)
     } 
+  } */
+
+  onLogin(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this._loginService.login(this.login.user_email, this.login.password)
+        .then(response => {
+          // Si llega aquí, fue login exitoso.
+        })
+        .catch(err => {
+          // Aquí capturas errores (401, 400, etc.)
+          if (err.status === 401) {
+            this.errorMessage = 'Credenciales inválidas';
+          } else if (err.status === 400) {
+            this.errorMessage = 'Faltan datos en el formulario';
+          } else {
+            this.errorMessage = 'Ocurrió un error inesperado';
+          }
+        });
+    }
   }
+
 }
